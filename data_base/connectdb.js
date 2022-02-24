caf46a78c039d75e6add238591836d5a9dca2a29
carloskauan <89313841+carloskauan@users.noreply.github.com>
2022-02-23 21:29:50 -0300

    Hekoku Modfys

--- data_base/connectdb.js
+++ data_base/connectdb.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 const connect = new Sequelize(
-    'askme',
-    'carloskauan',
-    '15971597ck',
+    'd5q1a96tf5kk69',
+    'dydsmpudcwivqp',
+    'f1dfd7fc18fccaed308b810a43cab8746ed214cc63b060212b74207a4c504ec5',
     {
-        root: 'localhost',
-        dialect: 'mysql'
+        root: 'c2-34-204-127-36.compute-1.amazonaws.com',
+        dialect: 'postgres'
     }
 );
 
